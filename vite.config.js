60b090fa98c809be35f263df0b861af54dc0747e
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-12 03:37:02 +0530

    package.json commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Travel-Booking-Tool-Frontend",
   plugins: [react(), tailwindcss()],
 });
