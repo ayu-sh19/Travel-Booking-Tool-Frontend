49bb0eb6ce658a8cb2b544460638b8558de62ac4
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-12 11:30:14 +0530

    renaming class to className

--- src/pages/HotelList.jsx
+++ src/pages/HotelList.jsx
@@ -48,15 +48,15 @@ function HotelList() {
         <div >
           <p className="ml-0 text-3xl pt-3 text-gray-900  font-mono">Hotel Offers</p>
         </div>
-        <ul class="max-w-lvh m-6 divide-y dark:divide-transparent divide-black  flex flex-col">
+        <ul className="max-w-lvh m-6 divide-y dark:divide-transparent divide-black  flex flex-col">
           {hotels.map((hotel, index) => (
-            <li key={index} class="pb-3 sm:pb-4 ">
-              <div class="flex items-center space-x-4 rtl:space-x-reverse dark:bg-gray-900 mx-auto dark:border-gray-700 h-auto p-7 rounded-2xl">
-                <div class="flex-1 min-w-0">
-                  <p class="text-xl font-medium text-gray-900 truncatetext-gray-900 dark:text-white">
+            <li key={index} className="pb-3 sm:pb-4 ">
+              <div className="flex items-center space-x-4 rtl:space-x-reverse dark:bg-gray-900 mx-auto dark:border-gray-700 h-auto p-7 rounded-2xl">
+                <div className="flex-1 min-w-0">
+                  <p className="text-xl font-medium text-gray-900 truncatetext-gray-900 dark:text-white">
                     {hotel.hotel.name}
                   </p>
-                  <p class="text-smtext-gray-900 dark:text-white truncate dark:text-gray-400">
+                  <p className="text-smtext-gray-900 dark:text-white truncate dark:text-gray-400">
                     <span className="text-sm">
                       {hotel.offers[0].price.currency}
                     </span>{" "}
@@ -65,7 +65,7 @@ function HotelList() {
                     </span>
                   </p>
                 </div>
-                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                   <Button
                     type="button"
                     variant="contained"
